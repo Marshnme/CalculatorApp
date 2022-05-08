2f6c15300a8c3f856848e3481cd2cabd7603baf8
Joshua Holtsclaw <jildsmiles@yahoo.com>
2022-05-07 20:21:17 -0400

    float values are limited to two  digits after decimal

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function updateInput(newValue){
     console.log("updatedInputValue",newValue)
     if(newValue.length === 0 ){
         input.value = 0;
-    }else if( typeof newValue !== typeof 1){
+    }else if( typeof newValue === typeof []){
         input.value = newValue.join("")
     }else{
         input.value = newValue
@@ -75,39 +75,59 @@ let operators = document.querySelectorAll(".operators span")
 operators.forEach(operator => {
     // operator.addEventListener("mousedown",inputOneToInputTwo)
     operator.addEventListener("mousedown",mathLogic)
-    if(operator.innerText != "="){
-        operator.addEventListener("mousedown",updateOperator)
-    }
 })
 
-function updateOperator(e){  
-    // inputs.operator = e.target.innerText
-}
+function isInt(n) {
+    return n % 1 === 0;
+ }
+
 
 function mathLogic(e){
     e.preventDefault()
     if(inputs.inputTwo.length != 0 ){
         if(e.target.innerText === "="){
             answers = results(inputs.inputTwo,inputs.inputOne,inputs.operator)
-            updateInput(answers)
-            inputs = {
-                inputTwo:[],
-                inputOne:[answers],
+            if(isInt(answers) === false){
+                let roundedAnswer = roundTo(answers,2)
+                updateInput(roundedAnswer)
+                inputs = {
+                    inputTwo:[],
+                    inputOne:[roundedAnswer],
+                    operator:e.target.innerText,
+                }
+            }else{
+                updateInput(answers)
+                inputs = {
+                    inputTwo:[],
+                    inputOne:[answers],
+                    operator:e.target.innerText,
+                }
             }
+            
         }else{
             answers = [results(inputs.inputTwo,inputs.inputOne,inputs.operator)]
-            inputs = {
-                inputTwo:[],
-                inputOne:[answers],
-                operator:e.target.innerText,
+            if(isInt(answers) === false){
+                let roundedAnswer = roundTo(answers,2)
+                inputs = {
+                    inputTwo:[],
+                    inputOne:[roundedAnswer],
+                    operator:e.target.innerText,
+                }
+                inputOneToInputTwo(e.target.innerText)
+                updateInput(roundedAnswer)
+            }else{
+                inputs = {
+                    inputTwo:[],
+                    inputOne:[answers],
+                    operator:e.target.innerText,
+                }
+            
+                inputOneToInputTwo(e.target.innerText)
+                console.log(inputs)
+                updateInput(answers)
             }
             
             
-        
-            
-            inputOneToInputTwo(e.target.innerText)
-            console.log(inputs)
-            updateInput(answers)
         }
     }else{
         inputOneToInputTwo(e.target.innerText)
@@ -138,6 +158,25 @@ function multiply(a,b){
 }
 function divide(a,b){
     return parseInt(a.join("")) / parseInt(b.join(""))
+}
+
+// rounding decimals func
+function roundTo(n, digits) {
+    var negative = false;
+    if (digits === undefined) {
+        digits = 0;
+    }
+    if (n < 0) {
+        negative = true;
+        n = n * -1;
+    }
+    var multiplicator = Math.pow(10, digits);
+    n = parseFloat((n * multiplicator).toFixed(11));
+    n = (Math.round(n) / multiplicator).toFixed(digits);
+    if (negative) {
+        n = (n * -1).toFixed(digits);
+    }
+    return n;
 }
 
 console.log("answers",answers)
